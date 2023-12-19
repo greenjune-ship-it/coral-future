7c964755b52c201377dff1c096a198a48fb4a38b
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-12-19 14:49:04 +0100

    fixed logout functionality

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # Add more URLs as needed
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+

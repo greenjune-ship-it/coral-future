a0dc5f2efb159dd1141360244f7da4aa8f461e41
Yulia Iakovleva <yulia_iakovleva@proton.me>
2023-12-29 19:55:30 +0100

    remove login requirement for now

--- django_app/projects/views.py
+++ django_app/projects/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from projects.models import Project, Sample, Observation
 
-@login_required
+#@login_required
 def project_list(request):
     projects = Project.objects.all()
     return render(request, 'projects/project_list.html', {'projects': projects})
 
-@login_required
+#@login_required
 def project_detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     # Get the count of samples for the project

035b7f06120719b2c0fec12b370d949644d2124a
Yulia Iakovleva <yulia_iakovleva@proton.me>
2023-12-29 18:50:16 +0100

    make url nicer

--- django_app/projects/urls.py
+++ django_app/projects/urls.py
@@ -3,5 +3,5 @@ from .views import project_list, project_detail
 
 urlpatterns = [
     path('', project_list, name='projects'),
-    path('projects/<int:project_id>/', project_detail, name='project_detail'),
+    path('project/<int:project_id>/', project_detail, name='project_detail'),
 ]

084124c26417c43cfc9a9c570d6bea76cdebb6f7
Yulia Iakovleva <yulia_iakovleva@proton.me>
2023-12-29 18:45:31 +0100

    changed map view to have project_id

--- django_app/maps/views.py
+++ django_app/maps/views.py
@@ -6,5 +6,18 @@ def map_view(request):
     # Retrieve all samples from the database
     samples = Sample.objects.all()
 
+    # Create a dictionary to store sample data with project_id
+    sample_data = []
+
+    # Fetch project_id for each sample and create a list of dictionaries
+    for sample in samples:
+        project_id = sample.project.id if sample.project else None  # Assuming 'project' is the ForeignKey in Sample model
+        sample_data.append({
+            'id': sample.id,
+            'latitude': sample.latitude,
+            'longitude': sample.longitude,
+            'project_id': project_id,
+        })
+
     # Pass sample data to the template
     return render(request, 'map.html', {'samples': samples})

962f56572b4722d3582a306639bfcc4c61dff619
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-01-22 20:09:55 +0100

    refactor urls

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('user/', include('users.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('maps/', include('maps.urls')),
     path('projects/', include('projects.urls')),
+    path('user/', include('users.urls')),
 ]

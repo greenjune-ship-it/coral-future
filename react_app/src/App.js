755d46b4fa7e8e86864cd250b4c3a32dcfcbfd8b
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-01-23 16:55:51 +0100

    created react entry point

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,46 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Cookies from 'js-cookie';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './AppNavbar';
 
 const App = () => {
-    const [authStatus, setAuthStatus] = useState({});
+  const [authStatus, setAuthStatus] = useState({});
 
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/check-authentication/', {
-                    withCredentials: true,
-                });
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/check-authentication/', {
+          withCredentials: true,
+        });
 
-                console.log('Response:', response);
+        // Parse the response data
+        const { username, is_authenticated } = response.data;
 
-                // Parse the response data
-                const { username, is_authenticated } = response.data;
+        // Set the authentication status and username
+        setAuthStatus({
+          username: username,
+          is_authenticated: is_authenticated,
+        });
+      } catch (error) {
+        console.error('Error checking authentication:', error);
+      }
+    };
 
-                // Set the authentication status and username
-                setAuthStatus({
-                    username: username,
-                    is_authenticated: is_authenticated,
-                });
-            } catch (error) {
-                console.error('Error checking authentication:', error);
-            }
-        };
+    checkAuthentication();
+  }, []);
 
-        checkAuthentication();
-    }, []);
-
-    return (
-        <div>
-            <h1>Authentication Status:</h1>
-            {authStatus.is_authenticated ? (
-                <p>Welcome, {authStatus.username}!</p>
-            ) : (
-                <p>You are not authenticated. Please set the session ID.</p>
-            )}
-            {/* Your other React components */}
-        </div>
-    );
+  return (
+    <div>
+      <AppNavbar authStatus={authStatus} />
+      <Container className="text-center mt-4">
+        <h1>Authentication Status:</h1>
+        {authStatus.is_authenticated ? (
+          <p>Welcome, {authStatus.username}!</p>
+        ) : (
+          <p>You are not authenticated. Please set the session ID.</p>
+        )}
+        {/* Your other React components */}
+      </Container>
+    </div>
+  );
 };
 
 export default App;

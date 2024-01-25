ec127f5a2013a2d3a1963bd53e8058b216be5c1d
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-01-25 19:03:10 +0100

    improved version of db populaiton

--- django_app/populate_db.py
+++ django_app/populate_db.py
@@ -1,6 +1,8 @@
 import os
 import django
 import pandas as pd
+import argparse
+from datetime import datetime
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
@@ -9,17 +11,18 @@ django.setup()
 # Import your models after setting up Django
 from projects.models import Project, Sample, Experiment, Observation
 from users.models import CustomUser
-from datetime import datetime
 
-def parse_and_create_instances(csv_path):
+def parse_and_create_instances(csv_path, owner_username):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(csv_path)
+
+    owner = CustomUser.objects.get(username=owner_username)
 
     project = Project.objects.create(
         name='Example Project',
         registration_date=datetime.now().date(),
         description='Project Description',
-        owner=CustomUser.objects.get(id=1)
+        owner=owner
     )
 
     # Iterate over unique experiments
@@ -57,10 +60,15 @@ def parse_and_create_instances(csv_path):
                 )
                 print(f"Created Observation: {observation}")
 
-if __name__ == "__main__":
-    # Provide the path to your CSV file
-    # csv_file_path = 'example.csv'
-    csv_file_path = 'static/datasheets/cbass_84.csv'
+def main():
+    parser = argparse.ArgumentParser(description='Parse CSV and create instances in Django models.')
+    parser.add_argument('--csv_path', type=str, required=True, help='Path to the input CSV file')
+    parser.add_argument('--owner', type=str, required=True, help='Username of the owner for the datasheet')
+
+    args = parser.parse_args()
 
     # Call the function to parse and create instances
-    parse_and_create_instances(csv_file_path)
+    parse_and_create_instances(args.csv_path, args.owner)
+
+if __name__ == "__main__":
+    main()

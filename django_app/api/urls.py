b1f8e007dda06d5fdb44c35d1f8546b3f400ff3d
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-01-25 13:42:52 +0100

    refactor is_authenticated method

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import CustomApiView
+from api.views import get_user_status, get_samples
 
 urlpatterns = [
-    path('check-authentication/', CustomApiView.as_view(), name='api-view'),
+    path('check-authentication/', get_user_status, name='api-view'),
+    path('samples/', get_samples, name='get-samples'),
 ]
--- django_app/api/views.py
+++ django_app/api/views.py
@@ -5,15 +5,29 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-class CustomApiView(View):
-    @method_decorator(csrf_exempt)
-    @method_decorator(login_required)
-    def get(self, request, *args, **kwargs):
-        # If the code reaches here, the user is authenticated
-        user = request.user
-        # Your logic for handling the authenticated user goes here
-        response_data = {
-            'username': user.username,
-            'is_authenticated': True,
-        }
-        return JsonResponse(response_data)
+from projects.models import Sample
+
+
+@method_decorator(csrf_exempt)
+def get_user_status(request, *args, **kwargs):
+    # If the code reaches here, the user is authenticated
+    user = request.user
+    # Your logic for handling the authenticated user goes here
+    response_data = {
+        'username': user.username,
+        'is_authenticated': True,
+    }
+    return JsonResponse(response_data)
+
+
+@method_decorator(login_required)
+def get_samples(request):
+    samples = Sample.objects.values(
+        'project_id',  # Use 'project_id' to get the ID of the related Project
+        'country',
+        'species',
+        'latitude',
+        'longitude',
+        'collection_date'
+    )
+    return JsonResponse(list(samples), safe=False)

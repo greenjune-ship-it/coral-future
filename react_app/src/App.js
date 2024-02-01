c8a7e816421fed955ff31c4e107873ec14b7fe0a
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-02-01 20:49:20 +0100

    reformat file

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   const [authStatus, setAuthStatus] = useState({});
   const [biosamples, setBiosamples] = useState([]);
   const [filters, setFilters] = useState({});
-    // Access the backend URL from the environment variable
+  // Access the backend URL from the environment variable
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
   useFetchAuthentication(backendUrl, setAuthStatus);
@@ -29,23 +29,23 @@ const App = () => {
   };
 
   return (
-          <div>
-                <NavigationBar authStatus={authStatus} />
-        <Container className="text-center mt-4">
-          <h1>BioSamples Explorer</h1>
-          <Row>
-            <Col md={3}>
-              {/* Pass handleApplyFilters function as a prop */}
-              <InputSidebar onApplyFilters={handleApplyFilters} speciesList={ [...new Set(biosamples.map(biosample => biosample.species))].sort()} />
-            </Col>
-            <Col md={9} style={{height: '600px'}}>
-              {/* Pass filters state as a prop */}
-              <Map filters={filters} biosamples={biosamples} />
-            </Col>
-          </Row>
-        </Container>
-              </div>
-      );
+    <div>
+      <NavigationBar authStatus={authStatus} />
+      <Container className="text-center mt-4">
+        <h1>BioSamples Explorer</h1>
+        <Row>
+          <Col md={3}>
+            {/* Pass handleApplyFilters function as a prop */}
+            <InputSidebar onApplyFilters={handleApplyFilters} speciesList={[...new Set(biosamples.map(biosample => biosample.species))].sort()} />
+          </Col>
+          <Col md={9} style={{ height: '600px' }}>
+            {/* Pass filters state as a prop */}
+            <Map filters={filters} biosamples={biosamples} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default App;

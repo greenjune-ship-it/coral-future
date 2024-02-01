ac3a4ea6330349663681980b4d1bd4848450f02b
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-02-01 20:45:04 +0100

    reorganized imports

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,18 +3,20 @@ import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Internal imports
-import Map from './pages/Map/Map';
-import NavigationBar from './components/Navbar/Navbar';
-import InputSidebar from './components/Sidebar/Sidebar';
+// Pages
+import Map from 'pages/Map/Map';
+// Hooks
 import useFetchAuthentication from './hooks/useFetchAuthentication';
 import useFetchBiosamples from './hooks/useFetchBiosamples';
-
+// Components
+import NavigationBar from 'components/Navbar/Navbar';
+import InputSidebar from 'components/Sidebar/Sidebar';
 
 const App = () => {
   const [authStatus, setAuthStatus] = useState({});
   const [biosamples, setBiosamples] = useState([]);
   const [filters, setFilters] = useState({});
-  // Access the backend URL from the environment variable
+    // Access the backend URL from the environment variable
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
   useFetchAuthentication(backendUrl, setAuthStatus);
@@ -27,23 +29,23 @@ const App = () => {
   };
 
   return (
-    <div>
-      <NavigationBar authStatus={authStatus} />
-      <Container className="text-center mt-4">
-        <h1>BioSamples Explorer</h1>
-        <Row>
-          <Col md={3}>
-            {/* Pass handleApplyFilters function as a prop */}
-            <InputSidebar onApplyFilters={handleApplyFilters} speciesList={ [...new Set(biosamples.map(biosample => biosample.species))].sort()} />
-          </Col>
-          <Col md={9} style={{height: '600px'}}>
-            {/* Pass filters state as a prop */}
-            <Map filters={filters} biosamples={biosamples} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+          <div>
+                <NavigationBar authStatus={authStatus} />
+        <Container className="text-center mt-4">
+          <h1>BioSamples Explorer</h1>
+          <Row>
+            <Col md={3}>
+              {/* Pass handleApplyFilters function as a prop */}
+              <InputSidebar onApplyFilters={handleApplyFilters} speciesList={ [...new Set(biosamples.map(biosample => biosample.species))].sort()} />
+            </Col>
+            <Col md={9} style={{height: '600px'}}>
+              {/* Pass filters state as a prop */}
+              <Map filters={filters} biosamples={biosamples} />
+            </Col>
+          </Row>
+        </Container>
+              </div>
+      );
 };
 
 export default App;

0d006d36e1b0224e777cc6c20db9967d29ef78d0
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-02-02 14:46:51 +0100

    style change

--- react_app/src/contexts/AuthContext.js
+++ react_app/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { useState, useEffect, createContext } from "react";
+import axios from 'axios';
+import React, { useState, useEffect, createContext } from 'react';
 
 export const AuthContext = createContext();
 

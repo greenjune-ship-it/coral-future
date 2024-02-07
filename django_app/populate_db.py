350bc75b8f8a3c87269cccaf3b22c94fd2da4ce6
Yulia Iakovleva <yulia_iakovleva@proton.me>
2024-02-07 19:46:03 +0100

    rebase main

--- django_app/populate_db.py
+++ django_app/populate_db.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import os
-from datetime import datetime
 
 import django
 import pandas as pd
@@ -11,80 +10,73 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 django.setup()
 
 # Import your models after setting up Django
-from projects.models import BioSample, Colony, Experiment, Observation, \
-    Project, Publication
 from users.models import CustomUser
+from management_scripts.create_objecs import create_biosample, create_colony, \
+    create_experiment, create_observation, create_project, create_publication
 
 # Configure the logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def parse_and_create_instances(csv_path, owner_username):
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(csv_path)
+def parse_csv(csv_path):
+    """
+    Parse the CSV file into a pandas DataFrame.
 
-    owner = CustomUser.objects.get(username=owner_username)
+    Args:
+        csv_path (str): Path to the CSV file.
 
+    Returns:
+        pd.DataFrame: DataFrame containing CSV data.
+    """
+    return pd.read_csv(csv_path)
+
+
+def create_instances_from_df(df, owner):
+    """
+    Create instances from DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing data.
+        owner (CustomUser): Owner of the data.
+
+    Returns:
+        None
+    """
     for project_key, project_group in df.groupby('Project.name'):
-
-        project, created = Project.objects.get_or_create(
-            name=project_key,
-            registration_date=datetime.now().date(),
-            description='Datasheet cbass_84.csv',
-            owner=owner
-        )
+        project, created = create_project(owner, project_key)
         logging.info(f"{project}, created: {created}")
 
         for experiment_key, experiment_group in project_group.groupby(
                 ['Experiment.name', 'Experiment.date']):
-            experiment, created = Experiment.objects.get_or_create(
-                name=experiment_key[0],
-                project=project,
-                date=datetime.strptime(experiment_key[1],
-                                       '%Y-%m-%d').date())
+            experiment, created = create_experiment(project, experiment_key)
             logging.info(f"{experiment}, created: {created}")
 
             for colony_key, colony_group in experiment_group.groupby(
                     ['Colony.name', 'Colony.species', 'Colony.country',
                      'Colony.latitude', 'Colony.longitude'
                      ]):
-                colony, created = Colony.objects.get_or_create(
-                    name=colony_key[0],
-                    species=colony_key[1],
-                    country=colony_key[2],
-                    latitude=colony_key[3],
-                    longitude=colony_key[4])
+                colony, created = create_colony(colony_key)
                 logging.info(f"{colony}, created: {created}")
 
                 for biosample_key, biosample_group in colony_group.groupby(
                         ['BioSample.name', 'BioSample.collection_date']):
-                    biosample, created = BioSample.objects.get_or_create(
-                        name=biosample_key[0],
-                        collection_date=biosample_key[1],
-                        colony=colony)
+                    biosample, created = create_biosample(colony,
+                                                          biosample_key)
                     logging.info(f"{biosample}, created: {created}")
 
                     for _, row in biosample_group.iterrows():
-                        observation, created = Observation.objects.get_or_create(
-                            biosample=biosample,
-                            experiment=experiment,
-                            condition=row['Observation.condition'],
-                            temperature=row['Observation.temperature'],
-                            timepoint=row['Observation.timepoint'],
-                            pam_value=row['Observation.pam_value'])
+                        print(row)
+                        observation, created = create_observation(biosample,
+                                                                  experiment,
+                                                                  row)
                         logging.info(f"{observation}, created: {created}")
 
-                        publication, created = Publication.objects.get_or_create(
-                            title=row['Publication.title'],
-                            year=row['Publication.year'],
-                            doi=row['Publication.doi']
-                        )
-
-                        observation.publications.add(publication)
-                        publication.projects.add(project)
-
+                        publication, created = create_publication(row, project)
                         logging.info(f"{publication}, created: {created}")
+
+                        # Create Many-to-Many relations
+                        observation.publications.add(publication)
 
 
 def main():
@@ -97,8 +89,14 @@ def main():
 
     args = parser.parse_args()
 
-    # Call the function to parse and create instances
-    parse_and_create_instances(args.csv_path, args.owner)
+    # Call the function to parse CSV
+    df = parse_csv(args.csv_path)
+
+    # Get owner
+    owner = CustomUser.objects.get(username=args.owner)
+
+    # Call the function to create instances
+    create_instances_from_df(df, owner)
 
 
 if __name__ == "__main__":

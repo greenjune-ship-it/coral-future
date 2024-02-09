625c2cec0fcd0c1c672b4714c61d79c94b67ef98
greenjune-ship-it <yulia_iakovleva@proton.me>
2024-02-09 17:40:00 +0100

    adjust serializer for colony

--- django_app/api/serializers.py
+++ django_app/api/serializers.py
@@ -9,10 +9,18 @@ class BioSampleSerializer(serializers.ModelSerializer):
 
 
 class ColonySerializer(serializers.ModelSerializer):
+    projects = serializers.SerializerMethodField()
+
+    def get_projects(self, obj):
+        # Get the related projects for the colony's biosamples
+        biosamples = obj.biosamples.all()
+        projects = Project.objects.filter(biosamples__in=biosamples).distinct()
+        # Assuming you want to serialize projects' names
+        return [project.name for project in projects]
+
     class Meta:
         model = Colony
-        fields = '__all__'
-
+        fields = ['id', 'name', 'species', 'country', 'latitude', 'longitude', 'ed50_value', 'projects']
 
 class ObservationSerializer(serializers.ModelSerializer):
     class Meta:

953cec53cfff69c4c60d31e287df540aa41a5fa4
Yulia Iakovleva <yulia_iakovleva@proton.me>
2024-04-14 14:20:05 +0200

    remove unused django app

--- django_app/maps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from maps.views import map_view
-
-urlpatterns = [
-    path('', map_view, name='maps'),  # Use /map for the map view
-    # Add other URL patterns as needed
-]
